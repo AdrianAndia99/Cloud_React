eb01b63530e46dd8fbca03c314fa847340fce6bd
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-12 21:49:03 -0500

    juego - mala resolucion

--- src/pages/Game2.tsx
+++ src/pages/Game2.tsx
@@ -1,9 +1,23 @@
+import {Unity, useUnityContext} from "react-unity-webgl";
+
 function Game2() {
-    return (
-      <div>
-        <h1>Game 2</h1>
-        <p>This is the second game.</p>
-      </div>
-    );
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/WEBGL.loader.js",
+    dataUrl: "/WEBGL.data",
+    frameworkUrl: "/WEBGL.framework.js",
+    codeUrl: "/WEBGL.wasm",
+});
+
+return (
+    <>
+        <div className="centered-container">
+            <div className="centered-content">
+                <h1 className="centered-title">Game 2</h1>
+                <Unity unityProvider={unityProvider} className="centered-unity" />
+            </div>
+        </div>
+
+    </>
+);
   }
   export default Game2;
