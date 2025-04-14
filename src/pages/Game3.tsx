4dd6b9bff4995c58004f4ad60a8b4b76ccd45dc2
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-14 15:59:20 -0500

    game3 redi

--- src/pages/Game3.tsx
+++ src/pages/Game3.tsx
@@ -1,9 +1,25 @@
+import {Unity, useUnityContext} from "react-unity-webgl";
+
 function Game3() {
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "/MemoryCards.loader.js",
+        dataUrl: "/MemoryCards.data",
+        frameworkUrl: "/MemoryCards.framework.js",
+        codeUrl: "/MemoryCards.wasm",
+    });
+
     return (
-      <div>
-        <h1>Game 3</h1>
-        <p>This is the third game.</p>
-      </div>
+        <>
+            <div className="centered-container">
+                <div className="centered-content">
+                    <h1 className="centered-title">React + Unity / Tecsup</h1>
+                    <Unity unityProvider={unityProvider} className="centered-unity" />
+                </div>
+            </div>
+
+        </>
     );
-  }
-  export default Game3;
+}
+
+
+export default Game3
