b21cca59dc01f06890dcab24168219123ac77094
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-14 16:16:23 -0500

    restart scene

--- src/pages/Game3.tsx
+++ src/pages/Game3.tsx
@@ -1,12 +1,16 @@
 import {Unity, useUnityContext} from "react-unity-webgl";
 
 function Game3() {
-    const { unityProvider } = useUnityContext({
+    const { unityProvider,sendMessage } = useUnityContext({
         loaderUrl: "/MemoryCards.loader.js",
         dataUrl: "/MemoryCards.data",
         frameworkUrl: "/MemoryCards.framework.js",
         codeUrl: "/MemoryCards.wasm",
     });
+
+    function handleClickSpawnEnemies() {
+      sendMessage("SceneManager", "Restart");
+  }
 
     return (
         <>
@@ -14,6 +18,9 @@ function Game3() {
                 <div className="centered-content">
                     <h1 className="centered-title">React + Unity / Tecsup</h1>
                     <Unity unityProvider={unityProvider} className="centered-unity" />
+                    <div className="centered-content">
+                        <button onClick={handleClickSpawnEnemies}>Restart Game</button>
+                    </div>
                 </div>
             </div>
 
