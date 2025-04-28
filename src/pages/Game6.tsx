0f970bdd862b9ca4fd5e03426977e95a6a3750fe
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-28 17:24:29 -0500

    gaaaaaaaaa

--- src/pages/Game6.tsx
+++ src/pages/Game6.tsx
@@ -1,9 +1,23 @@
+import {Unity, useUnityContext} from "react-unity-webgl";
+
 function Game6() {
-    return (
-      <div>
-        <h1>Game 6</h1>
-        <p>This is el 6to game.</p>
-      </div>
-    );
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/WEBGL.loader.js",
+    dataUrl: "/WEBGL.data",
+    frameworkUrl: "/WEBGL.framework.js",
+    codeUrl: "/WEBGL.wasm",
+});
+
+return (
+    <>
+        <div className="centered-container">
+            <div className="centered-content">
+                <h1 className="centered-title">Game 2</h1>
+                <Unity unityProvider={unityProvider} className="centered-unity" />
+            </div>
+        </div>
+    </>
+);
   }
   export default Game6;
+  // falta meter el juego de construct
