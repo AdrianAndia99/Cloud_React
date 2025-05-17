e1576725b25f47c283b2c0bb67ab4818274ea2a2
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-05-17 12:44:54 -0500

    game4

--- src/pages/Game5.tsx
+++ src/pages/Game5.tsx
@@ -1,9 +1,54 @@
+import { useState, useEffect } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
 function Game5() {
-    return (
-      <div>
-        <h1>Game 5</h1>
-        <p>This is the fifth game.</p>
+  const { unityProvider, sendMessage } = useUnityContext({
+    loaderUrl: "/navecita.loader.js",
+    dataUrl: "/navecita.data",
+    frameworkUrl: "/navecita.framework.js",
+    codeUrl: "/navecita.wasm",
+  });
+
+  const [name, setName] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  /*const sendName = () => {
+    console.log("Nombre enviado a Unity:", name);
+    sendMessage("SceneManager", "ChangeText", name);
+  };*/
+
+ // const resetScene = () => {
+ //   sendMessage("Controller", "loadScene");
+ // };
+
+  useEffect(() => {
+    const canvas = document.querySelector("canvas");
+    if (canvas) {
+      canvas.setAttribute("tabindex", "-1"); // evita que Unity capture el teclado
+    }
+  }, []);
+
+  return (
+    <div className="centered-container">
+      <div className="centered-content">
+        <h1 className="centered-title">React + Unity / Tecsup</h1>
+        <Unity unityProvider={unityProvider} className="centered-unity" tabIndex={1} />
+
+        <div className="input-container" style={{ marginTop: "10px" }}>
+          <input
+            type="text"
+            value={name}
+            onChange={handleInputChange}
+            placeholder="Escribe tu nombre"
+            style={{ padding: "5px", marginRight: "5px" }}
+          />
+        </div>
       </div>
-    );
-  }
-  export default Game5;
+    </div>
+  );
+}
+
+export default Game5;
